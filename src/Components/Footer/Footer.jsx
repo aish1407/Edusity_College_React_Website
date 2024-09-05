f55e2c30127c0459c11800d8b14cfa6a8449c9c2
aish1407 <139777141+aish1407@users.noreply.github.com>
2024-09-05 10:34:58 +0530

    Add files via upload

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Footer.css'
+const Footer = () => {
+  return (
+    <div className='Footer'>
+      <p>@ 2024 University.All rights reserved</p>
+      <ul>
+        <li>Terms of service</li>
+        <li>Privacy policy</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Footer
